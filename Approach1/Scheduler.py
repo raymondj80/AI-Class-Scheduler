5a223c9e55f461c2732e6432ac8431ea638388cf
raymondj80 <55035695+raymondj80@users.noreply.github.com>
2020-12-08 19:46:21 -0600

    Update Scheduler.py

--- Approach1/Scheduler.py
+++ Approach1/Scheduler.py
@@ -116,14 +116,6 @@ class Scheduler:
         hour = time % 24
         return "{}:{}".format(day,hour)
 
-# Test Case 3
-class1 = ['PHYS16','MWF:12-13','TuWTh:15-18',6,'F:18',0.30]
-class2 = ['CS182','MWF:14-15','MW:15-19',8,'Th:18',0.15]
-class3 = ['GENED1023','TuTh:12-14','MW:12-13',2,'W:18',0.15]
-work_ = 'MTuWThFSatSun:12-20'
-
-
-
 
 
 
