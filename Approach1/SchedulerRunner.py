04cd1b6f5b607e796717f97737b9cadc279790d7
raymondj80 <55035695+raymondj80@users.noreply.github.com>
2020-12-09 10:36:24 -0600

    Final Version

--- Approach1/SchedulerRunner.py
+++ Approach1/SchedulerRunner.py
@@ -1,17 +1,19 @@
 from Scheduler import Scheduler
+S = Scheduler()
 
 # Modify Class Data and Work Schedule
-# class1 = ['PHYS16','MWF:12-13','TuWTh:15-18',6,'F:18',0.30]
-class2 = ['CS182','MWF:14-15','MW:15-19',8,'Th:18',0.30]
-# class3 = ['GENED1023','TuTh:12-14','MW:12-13',2,'W:18',0.15]
+class1 = ['PHYS16','MWF:12-16','MWTh:13-18',6,'F:18',0.30]
+class2 = ['CS182','MWF:14-15','TuTh:18-19',4,'W:12',0.30]
+class3 = ['GENED1023','TuTh:12-14','MW:12-13',1,'W:18',0.15]
 work_ = 'MTuWThF:12-18'
 
+# Modify Constraints (break_hours_between_work, total_work_hours_per_day)
+S.add_constraints(1,5)
+
 # Do not Modify
-D = Scheduler()
-# S.add_class(class1)
-D.add_class(class2)
-# S.add_class(class3)
-D.add_work_schedule(work_)
-D.add_constraints(1,5)
-F = D.back_tracking_search()
-D.print_final_schedule(F)
+S.add_class(class1)
+S.add_class(class2)
+S.add_class(class3)
+S.add_work_schedule(work_)
+F = S.back_tracking_search()
+S.print_final_schedule(F)
